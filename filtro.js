6a80e0312f6d35f2f63cc32f2dd0a39e8a46adb2
Gustavo Félix <99498636+gustavohfelixs@users.noreply.github.com>
2024-05-11 00:51:08 +0000

    novas informações da aula

---
+++ filtro.js
@@ -0,0 +1,25 @@
+var campoFilro = document.getElementById("filtra-tabela");
+
+campoFilro.addEventListener("input", function () {
+  var clientes = document.querySelectorAll(".clientes");
+
+  if (this.value.length > 0) {
+    for (var cliente = 0; cliente < clientes.length; cliente++) {
+      var nome = clientes[cliente].querySelector(".nome").textContent;
+
+      var expressao = new RegExp(this.value, "i");
+
+      if (!expressao.test(nome)) {
+        // console.log(clientes[cliente]);
+        clientes[cliente].classList.add("invisivel");
+      } else {
+        clientes[cliente].classList.remove("invisivel");
+      }
+    }
+  } else {
+    for (var cliente = 0; cliente < clientes.length; cliente++) {
+      var nome = clientes[cliente].querySelector(".nome").textContent;
+      clientes[cliente].classList.remove("invisivel");
+    }
+  }
+});
